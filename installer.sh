5695a01172c0600d88ee531c0b000b973fb86a61
Achmad Nashruddin Riskynanda <91010605+achmadnr21@users.noreply.github.com>
2024-11-26 14:51:12 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,10 +3,10 @@
 sudo apt install python3-opencv
 sleep 2
 # Menyalin direktori ke /usr/local/bin/
-sudo cp -r markaswalet-stream /usr/local/bin/
+sudo cp -rf markaswalet-stream /usr/local/bin/
 sleep 2
 # Menyalin file service ke /etc/systemd/system/
-sudo cp markaswalet-stream.service /etc/systemd/system/
+sudo cp -f markaswalet-stream.service /etc/systemd/system/
 sleep 2
 # Menginformasikan pengguna
 echo "File dan direktori telah berhasil disalin."
